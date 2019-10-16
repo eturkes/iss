5a8c4a450eee365629c52e4d2969b3f877a24a9e
jduffield65 <44460382+jduffield65@users.noreply.github.com>
2019-10-16 17:31:15 +0100

    Update load_3D.m
    Loads in tiffs slightly quicker.

--- @iss/load_3D.m
+++ @iss/load_3D.m
@@ -1,8 +1,20 @@
 function Image3D = load_3D(o,r,y,x,c)
 %given round,y,x index of tile and channel c, this returns the full 3D image.
 
+FileTif = o.TileFiles{r,y,x,c};
 Image3D = zeros(o.TileSz,o.TileSz,o.nZ);
-for z = 1:o.nZ
-    Image3D(:,:,z) = imread(o.TileFiles{r,y,x,c}, z);
+FileID = Tiff(FileTif,'r');
+rps = getTag(FileID,'RowsPerStrip');
+ 
+for z=1:o.nZ
+   setDirectory(FileID,z);
+   % Go through each strip of data.
+   rps = min(rps,o.TileSz);
+   for j = 1:rps:o.TileSz
+      row_inds = j:min(o.TileSz,j+rps-1);
+      stripNum = computeStrip(FileID,j);
+      Image3D(row_inds,:,z) = readEncodedStrip(FileID,stripNum);
+   end
 end
+close(FileID);
 end
