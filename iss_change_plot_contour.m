960affcaa44c6298955b3af086b08e7587b9bbe2
Emir Turkes <eturkes@bu.edu>
2020-07-30 17:22:22 -0400

    Match original code style and add documentation

--- iss_change_plot_contour.m
+++ iss_change_plot_contour.m
@@ -1,10 +1,11 @@
 function iss_change_plot_contour(o,Method,GenesToShow,UseSpots)
-%% iss_change_plot(o,Method,GenesToShow,UseSpots)
+%% iss_change_plot_contour(o,Method,GenesToShow,UseSpots)
 %
-% Given issPlot3DObject, this function lets you change the details
-% of the plot without closing the figure e.g. you can change
-% o.CombiQualThresh, o.pIntensityThresh, o.pScoreThresh, o.pScoreThresh2,
-% o.pLogProbThresh
+% This function is identical to iss_change_plot(), except that it includes
+% a semi-transparent contour plot overlayed onto the figure that shows the
+% density of spots. Note that the color map is not absolute, the itensity
+% of densities are not directly comparable between figures, although the
+% relative spatial distribution should be.
 %
 % o: iss object
 % Method: 'DotProduct','Prob' or 'Pixel' to consider gene assignments given
@@ -126,49 +127,38 @@ else
     set(gcf, 'InvertHardcopy', 'off');    
 end
 
-spot_no = find(ismember(o.GeneNames, GenesToShow));
-thresh = o.quality_threshold('Pixel') & ismember(o.pxSpotCodeNo, spot_no);
-
-spots = o.pxSpotGlobalYX(thresh,:);
-codes = o.pxSpotCodeNo(thresh,:);
-subset = size(spots);
-rng(1);
-subset = randsample(subset(1), subset(1));
-sub_spots = spots(subset,:);
-
-% The following section is adapted from content on Stack Overflow.
+% all code for the contour plots are from here until the end
+% the majority of which is adapted from content on Stack Overflow
 % https://stackoverflow.com/questions/9134014/contour-plot-coloured-by-clustering-of-points-matlab
 % Question asked by: HCAI https://stackoverflow.com/users/1134241/hcai
 % Answer given by: Vidar https://stackoverflow.com/users/346645/vidar
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+SpotNo = find(ismember(o.GeneNames, GenesToShow));
+Thresh = o.quality_threshold('Pixel') & ismember(o.pxSpotCodeNo, SpotNo);
+Spots = o.pxSpotGlobalYX(Thresh,:);
+
 Border = 0;
 Sigma = 500;
-stepSize = 100;
+StepSize = 100;
 
-X=(sub_spots(:,2) / 1)';
-Y=(sub_spots(:,1) / 1)';
+X = (Spots(:,2) / 1)';
+Y = (Spots(:,1) / 1)';
 D = [X' Y'];
 N = length(X);
 
 Xrange = [min(X)-Border max(X)+Border];
 Yrange = [min(Y)-Border max(Y)+Border];
 
-% Set up coordinate grid.
-%%%%%%%%%%%%%%%%%%%%%%%%%
-[XX, YY] = meshgrid(Xrange(1):stepSize:Xrange(2), Yrange(1):stepSize:Yrange(2));
+% set up coordinate grid
+[XX, YY] = meshgrid(Xrange(1):StepSize:Xrange(2), Yrange(1):StepSize:Yrange(2));
 YY = flipud(YY);
-%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Parzen parameters and function handle.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% parzen parameters and function handle
 pf1 = @(C1,C2) (1/N)*(1/((2*pi)*Sigma^2)).* ...
          exp(-( (C1(1)-C2(1))^2+ (C1(2)-C2(2))^2)/(2*Sigma^2));
 
 PPDF1 = zeros(size(XX));
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Populate coordinate surface.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% populate coordinate surface
 [R, C] = size(PPDF1);
 NN = length(D);
 for c = 1:C
@@ -179,34 +169,28 @@ for c = 1:C
        end
    end
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% Normalize data.
-%%%%%%%%%%%%%%%%%
+% normalize data
 m1 = max(PPDF1(:));
 PPDF1 = PPDF1 / m1;
-%%%%%%%%%%%%%%%%%
 
-% Add PDF estimates to figure.
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-orig_ax = gca;
-contour_ax = axes;
-[~, contour_h] = contourf(XX, YY, PPDF1);
+% add PDF estimates to figure
+OrigAx = gca;
+ContourAx = axes;
+[~, ContourH] = contourf(XX, YY, PPDF1);
 colormap(gca, 'hot')
 set(gca, 'YDir', 'reverse');
 set(gca, 'XDir', 'reverse');
 set(gca, 'color', 'none');
 set(gca,'XTick',[], 'YTick', []);
 
-drawnow; % Required for the below to work.
-fills_h = contour_h.FacePrims;
-[fills_h.ColorType] = deal('truecoloralpha');
-for i = 1:numel(fills_h)
-   fills_h(i).ColorData(4) = 100;
+drawnow; % Required for the below to work
+FillsH = ContourH.FacePrims;
+[FillsH.ColorType] = deal('truecoloralpha');
+for i = 1:numel(FillsH)
+   FillsH(i).ColorData(4) = 100;
 end
 
-set(gcf, 'currentaxes', orig_ax);
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+set(gcf, 'currentaxes', OrigAx);
 
 assignin('base','issPlot2DObject',S)
