016ed11662059a8a97ca88ccf5dfee5f1d714601
Emir Turkes <eturkes@bu.edu>
2020-10-14 18:54:41 +0100

    Adjust marker and font size

--- iss_change_plot_individual_MG133.m
+++ iss_change_plot_individual_MG133.m
@@ -120,7 +120,7 @@ legend off;
 set(gca, 'Clipping', 'off');
 
 if ~isempty(PlotSpots)
-    change_gene_symbols_individual_MG133(6, 12);
+    change_gene_symbols_individual_MG133(10, 25);
 else
     set(gcf, 'color', 'k');
     set(gcf, 'InvertHardcopy', 'off');    
